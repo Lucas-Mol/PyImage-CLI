7e8cc4966e6f3e0bf90e866482c35ebe276ce28d
Lucas-Mol <lucasmolpro@outlook.com>
2023-05-05 12:35:39 -0300

    refactor(transparentizer.py): cleaning code

--- image_modifiers/transparentizer.py
+++ image_modifiers/transparentizer.py
@@ -2,24 +2,35 @@ import common.msg_printer as msg_printer
 import common.image_utils as image_utils
 import image_modifiers.converter as converter
 
-_WHITE = 255
+_BARELY_WHITE = 240
 
 def _transparentizer_img(img):
     img = img.convert("RGBA")
     datas = img.getdata()
-    new_data = []
+    new_data = from_white_pixel_to_transp(datas)
 
-    for item in datas:
-        if item[0] == _WHITE and item[1] == _WHITE and item[2] == _WHITE:
-            new_data.append((_WHITE, _WHITE, _WHITE, 0))
-        else:
-            new_data.append(item)
     img.putdata(new_data)
     return img
 
+def _is_barely_white(data):
+    if data is None:
+        return True
+    else:
+        return data[0] >= _BARELY_WHITE and data[1] >= _BARELY_WHITE and data[2] >= _BARELY_WHITE
+
+def from_white_pixel_to_transp(datas):
+    new_data = []
+    for item in datas:
+        if _is_barely_white(item):
+            new_data.append((_BARELY_WHITE, _BARELY_WHITE, _BARELY_WHITE, 0))
+        else:
+            new_data.append(item)
+    return new_data
+
 def _save_img(img):
     file_name = "transparent_image"
-    output_file = image_utils.get_output_file(file_name)
+    desired_format = "png"
+    output_file = image_utils.get_output_file_ext(file_name, desired_format)
 
     img.save(output_file, format="PNG")
 
